75a439080e15be9d635fd63c5ea5c1041239f706
Yongseop Kim <Yongseop Kim>
2016-04-08 20:58:57 +0900

    [PayStory3] Migration files from PayStory2 to PayStory3

--- PayStoryKitKat3/app/src/main/java/com/bluehack/paystorykitkat3/MainActivity.java
+++ PayStoryKitKat3/app/src/main/java/com/bluehack/paystorykitkat3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bluehack.paystorykitkat3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setTitleTextColor(0xFF0000);
+        toolbar.setTitle("PayStory");
+        toolbar.setTitleTextColor(0xFF4E4E50);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setTabTextColors(0xFF4A4B4E, 0x40e9ff);
+
+        tabLayout.addTab(tabLayout.newTab().setText("����"));
+        tabLayout.addTab(tabLayout.newTab().setText("�����"));
+        tabLayout.addTab(tabLayout.newTab().setText("����"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
---
+++ PayStoryKitKat3/app/src/main/java/com/bluehack/paystorykitkat3/TabFragment3.java
@@ -0,0 +1,141 @@
+package com.bluehack.paystorykitkat3;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class TabFragment3 extends Fragment {
+    private ListView mListView = null;
+    private ListViewAdapter mAdapter = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.tab_fragment_3, container, false);
+
+        mListView = (ListView)layout.findViewById(R.id.listView);
+        mAdapter = new ListViewAdapter(getActivity());
+        mListView.setAdapter(mAdapter);
+/*
+        mAdapter.addItem(getResources().getDrawable(R.drawable.face),
+                        "Somebody talks to you", "2016-03-18");
+        mAdapter.addItem(getResources().getDrawable(R.drawable.face),
+                        "Notification!!", "2016-03-18");
+        mAdapter.addItem(getResources().getDrawable(R.drawable.face),
+                        "Hey!!!", "2016-03-18");
+        mAdapter.addItem(getResources().getDrawable(R.drawable.face),
+                        "Are you sleeping?", "2016-03-18");
+*/
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundResource(R.color.background_material_light);
+
+                ListData mData = mAdapter.mListData.get(position);
+                Toast.makeText(getActivity(), mData.mTitle, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return layout;
+    }
+
+    private class ListViewAdapter extends BaseAdapter {
+        private Context mContext = null;
+        private ArrayList<ListData> mListData = new ArrayList<ListData>();
+
+        public ListViewAdapter(Context mContext) {
+            super();
+            this.mContext = mContext;
+        }
+
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mListData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.tab_fragment_3_listview, null);
+
+                holder.mIcon = (ImageView) convertView.findViewById(R.id.mImage);
+                holder.mText = (TextView) convertView.findViewById(R.id.mText);
+                holder.mDate = (TextView) convertView.findViewById(R.id.mDate);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            ListData mData = mListData.get(position);
+
+            if (mData.mIcon != null) {
+                holder.mIcon.setVisibility(View.VISIBLE);
+                holder.mIcon.setImageDrawable(mData.mIcon);
+            } else {
+                holder.mIcon.setVisibility(View.GONE);
+            }
+
+            holder.mText.setText(mData.mTitle);
+            holder.mDate.setText(mData.mDate);
+
+            return convertView;
+        }
+
+        public void addItem(Drawable icon, String mTitle, String mDate){
+            ListData addInfo = null;
+            addInfo = new ListData();
+            addInfo.mIcon = icon;
+            addInfo.mTitle = mTitle;
+            addInfo.mDate = mDate;
+
+            mListData.add(addInfo);
+        }
+
+        public void remove(int position){
+            mListData.remove(position);
+            dataChange();
+        }
+
+        public void dataChange(){
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public class ListData {
+        public Drawable mIcon;
+        public String mTitle;
+        public String mDate;
+    }
+
+    private class ViewHolder {
+        public ImageView mIcon;
+        public TextView mText;
+        public TextView mDate;
+    }
+}
